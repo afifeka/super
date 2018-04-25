b6596c0daa6d890897d3a3485e8beefff73f2039
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-25 18:00:00 +0800

    Update MusicBot.js

--- MusicBot.js
+++ MusicBot.js
@@ -1,6 +1,6 @@
 const { Client, Util } = require('discord.js');
 const { PREFIX } = require('./config');
-const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
+const GOOGLE_API_KEY = process.env.API_KEY
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 
